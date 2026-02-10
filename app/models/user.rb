2481edd945a75559650eefa2acf0ffe756b8c9d9
Heather Forester <hforeste07@gmail.com>
2026-02-10 23:03:48 +0000

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :location
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :location, presence: true
 end
