987b259f77537d64eb7366902ed4298af8a50802
Heather Forester <hforeste07@gmail.com>
2026-02-24 17:06:05 +0000

    added default values for the garden plot

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -18,8 +18,21 @@
 #
 #  fk_rails_...  (user_id => users.id)
 #
+# app/models/garden.rb
 class Garden < ApplicationRecord
   belongs_to :user
   has_many :garden_plots, dependent: :destroy
-  has_many :plants, through: :garden_plots
+
+  after_create :generate_plots
+
+  def generate_plots
+    rows.times do |row|
+      columns.times do |col|
+        garden_plots.create!(
+          row_position: row,
+          column_position: col
+        )
+      end
+    end
+  end
 end
---
+++ db/migrate/20260224170449_add_default_grid_size_to_gardens.rb
@@ -0,0 +1,4 @@
+class AddDefaultGridSizeToGardens < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
